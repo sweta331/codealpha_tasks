523a9fb4734f422702de9944b8b8411b4111270a
Sweta Lodhi <swetalodhi796@gmail.com>
2026-04-17 14:38:59 +0530

    added chatbot project

---
+++ rule-based-chatbot/README.md
@@ -0,0 +1,29 @@
+# 💬 Rule-Based Chatbot with GUI
+This is a simple **Rule-Based Chatbot** built using Python.
+It responds to user inputs based on predefined rules and also includes a **Graphical User Interface (GUI)** created with Tkinter for a better user experience.
+## 🚀 Features
+* 🤖 Rule-based responses using if-elif logic
+* 💬 Interactive chat interface
+* 🔄 Continuous conversation loop
+## 🛠️ Technologies Used
+* Python
+* Tkinter (for GUI)
+## 📁 Project Structure:
+rule_based_chatbot/
+│── chatbot_gui.py              
+│── README.md
+## ▶️ How to Run
+1. Make sure Python is installed on your system
+2. Open the project folder in VS Code or terminal
+3. Run the GUI chatbot:
+python chatbot_gui.py
+## 💡 Example Inputs
+You can try:
+* hello
+* how are you
+* your name
+* bye
+## 👩‍💻 Author
+Sweta lodhi
+## ⭐ Note
+This project is beginner-friendly and demonstrates basic concepts of Python, loops, conditional statements, and GUI development.
---
+++ rule-based-chatbot/chatbot_gui.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+
+# chatbot logic 
+def chatbot_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hello there!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks for asking!"
+    elif user_input == "your name":
+        return "I am a chatbot"
+    elif user_input == "joke":
+        return "okay : Why did the computer catch a cold? Because it left its Windows open!"
+    elif user_input == "that's funny":
+        return "Yes!"
+    elif user_input in ["bye", "goodbye"]:
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand that!"
+
+# send message function
+def send_message():
+    user_input = entry.get()
+    chat_log.insert(tk.END, "You: " + user_input + "\n")
+
+    response = chatbot_response(user_input)
+    chat_log.insert(tk.END, "Bot: " + response + "\n\n")
+
+    entry.delete(0, tk.END)
+
+# GUI setup for chatbot
+root = tk.Tk()
+root.title("Chatbot")
+
+chat_log = tk.Text(root, height=20, width=50)
+chat_log.pack()
+
+entry = tk.Entry(root, width=50)
+entry.pack()
+
+send_btn = tk.Button(root, text="Send", command=send_message)
+send_btn.pack()
+
+root.mainloop()
